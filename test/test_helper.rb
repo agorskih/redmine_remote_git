059dc505c16f8a7975c54975f6d42aeb289ce91d
aggorskikh <alexandergorskih@iMac-Alexander.local>
2016-03-15 14:36:08 +0500

    working out broken simplecov installation 11.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,5 @@
-# Load codecov helper
 require 'simplecov'
-require 'simplecov-rcov'
 SimpleCov.start
-if ENV['CI']=='true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
