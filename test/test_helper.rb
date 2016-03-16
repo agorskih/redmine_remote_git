c73722d3d6559ae86ef9deb25ca776892742a786
gordev <alexander.gorskih@gmail.com>
2016-03-17 04:55:30 +0500

    add repositories initialized test to controller 14.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,8 @@ end
 
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+# Ensure that we are using the temporary fixture path
+class ActiveSupport::TestCase
+  self.fixture_path = File.dirname(__FILE__) + '/fixtures'
+end
