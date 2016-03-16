18b4c5611bbb7606b8ec2d9139a83220c8c92b10
aggorskikh <alexandergorskih@iMac-Alexander.local>
2016-03-17 02:42:36 +0500

    add assertion for repositories instance variable is set.

--- test/functional/git_repositories_controller_test.rb
+++ test/functional/git_repositories_controller_test.rb
@@ -1,11 +1,14 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class GitRepositoriesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
+  
+  def test_
+
+  def test_index
     get :index
 
     assert_response :success
     assert_template 'index'
+    assert_not_nil assigns(:repositories)
   end
 end
