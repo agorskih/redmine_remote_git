cd7d046cada4a9dcb1d3cd1755a2f4e17d77ab6c
gordev <alexander.gorskih@gmail.com>
2016-03-17 03:56:07 +0500

    add repositories initialized test to controller 4.

--- test/functional/git_repositories_controller_test.rb
+++ test/functional/git_repositories_controller_test.rb
@@ -12,8 +12,10 @@ class GitRepositoriesControllerTest < ActionController::TestCase
   def test_repositories_initialized_correctly
   	get :index
 
-  	assert_equal :repositories.to_a.count, 0
-  	repo = :repositories.to_a.first
+  	repos = assigns(:repositories)
+  	repo = repos.first
+
+  	assert_equal repos.count, 1
   	assert_equal repo.id, 1
   	assert_equal repo.remote_origin_url, "https://github.com/gordev/redmine_remote_git.git"
   	assert_equal repo.local_clone_path, "redmine_remote_git"
