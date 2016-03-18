05bef3521800edb67dede7afb3ed280902ecf17b
gordev <alexander.gorskih@gmail.com>
2016-03-19 02:15:53 +0500

    add resource routing 6.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-resources :git_repositories
-#get 'gitrepos', :to => 'git_repositories#index'
-#post 'repo', :to => 'git_repositories#create'
+resources :git_repositories, :only => [:index]
