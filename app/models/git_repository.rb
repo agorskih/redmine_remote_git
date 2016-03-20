7d6b377b96c18593af1e5a69a292e7b0978c5b8a
gordev <alexander.gorskih@gmail.com>
2016-03-20 23:31:16 +0500

    add repositoriy clone path uniqueness restriction.

--- app/models/git_repository.rb
+++ app/models/git_repository.rb
@@ -1,4 +1,5 @@
 class GitRepository < ActiveRecord::Base
   unloadable
   validates :remote_origin_url, :local_clone_path, presence: true
+  validates :local_clone_path, uniqueness: true
 end
--- test/unit/git_repository_test.rb
+++ test/unit/git_repository_test.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class GitRepositoryTest < ActiveSupport::TestCase
+	fixtures :git_repositories
+
+	def test_clone_path_should_be_unique
+		repo = GitRepository.new(remote_origin_url: 'https://github.com/gordev/redmine_remote_git.git', local_clone_path: 'redmine_remote_git')
+		refute repo.valid?
+	end
 
 	def test_local_clone_path_should_be_present
 		repo = GitRepository.new(remote_origin_url: 'https://github.com/gordev/redmine_remote_git.git')
@@ -8,7 +14,7 @@ class GitRepositoryTest < ActiveSupport::TestCase
 	end
 
   def test_remote_origin_url_should_be_present
-  	repo = GitRepository.new(local_clone_path: 'clone')
+  	repo = GitRepository.new(local_clone_path: 'redmine_remote_git')
     refute repo.valid?
   end
 end
