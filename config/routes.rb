159825efa5a99014044c3ee2ef84cb9a6ecbff47
gordev <alexander.gorskih@gmail.com>
2016-03-20 08:57:43 +0500

    add new page form test 25.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@
 # following implementation would not actually work because of a way redmine does not allow it
 # see: https://www.redmine.org/boards/1/topics/2885?r=17643
 
-get 'git_repositories', :to => 'git_repositories#index'
-get 'git_repositories/new', :to => 'git_repositories#new'
-post 'git_repositories', :to => 'git_repositories#create'
-
 resources :git_repositories, :only => [:index, :new]
