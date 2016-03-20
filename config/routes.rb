3056e93861edc8e4123285bd1e34424757bc1377
gordev <alexander.gorskih@gmail.com>
2016-03-20 08:11:38 +0500

    add new page form test 17.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-resources :git_repositories, :only => [:index, :new]
+# following implementation would not actually work because of a way redmine does not allow it
+# see: https://www.redmine.org/boards/1/topics/2885?r=17643
+# resources :git_repositories, :only => [:index, :new]
