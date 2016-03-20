1d85c8c7e733262c70acb90994719c8ca6feaca1
gordev <alexander.gorskih@gmail.com>
2016-03-20 15:17:05 +0500

    add new page form test 64.

--- app/controllers/git_repositories_controller.rb
+++ app/controllers/git_repositories_controller.rb
@@ -13,7 +13,7 @@ class GitRepositoriesController < ApplicationController
   def create
     fields = params[:repository]
     @repository = GitRepository.new
-    #@repository.remote_origin_url = fields['repository_remote_origin_url']
+    @repository.remote_origin_url = fields[:repository_remote_origin_url]
     #@repository.local_clone_path = fields['repository_local_clone_path']
     #@repository.save
   end
