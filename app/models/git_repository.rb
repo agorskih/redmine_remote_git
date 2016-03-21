d3a891b286c85cf87b867225e235f6713580028e
gordev <alexander.gorskih@gmail.com>
2016-03-21 11:56:35 +0500

    add repositoriy remote url must be valid 3.

--- app/models/git_repository.rb
+++ app/models/git_repository.rb
@@ -8,7 +8,9 @@ class GitRepository < ActiveRecord::Base
 
   def remote_origin_url_must_be_HTTP_or_HTTPS_URL
   	uri = URI.parse(remote_origin_url)
-  	uri.kind_of?(URI::HTTP)
+  	unless uri.kind_of?(URI::HTTP)
+  		raise URI::InvalidURIError
+  	end
 	rescue URI::InvalidURIError
 	  errors.add(:remote_origin_url, 'remote origin URL is not HTTP/HTTPS URL')
   end
