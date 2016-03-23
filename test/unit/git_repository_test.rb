7c2673fd5e717f402e0f564247b0d4aeff9efc87
gordev <alexander.gorskih@gmail.com>
2016-03-23 13:29:04 +0500

    add create display error 4.

--- test/unit/git_repository_test.rb
+++ test/unit/git_repository_test.rb
@@ -4,9 +4,10 @@ class GitRepositoryTest < ActiveSupport::TestCase
 	fixtures :git_repositories
 
 	def test_only_valid_url_should_be_allowd
-		repo = GitRepository.new(remote_origin_url: 'invalid://url', local_clone_path: 'redmine_remote_git')
+		repo = GitRepository.new(remote_origin_url: 'invalid://url', local_clone_path: 'redmine_remote_git copy')
 		refute repo.valid?
 		assert_equal(repo.errors[:remote_origin_url], ['remote origin URL is not HTTP/HTTPS URL'])
+		assert_equal(repo.errors[:local_clone_path], [''])
 	end
 
 	def test_clone_path_should_be_unique
@@ -20,7 +21,7 @@ class GitRepositoryTest < ActiveSupport::TestCase
 	end
 
   def test_remote_origin_url_should_be_present
-  	repo = GitRepository.new(local_clone_path: 'redmine_remote_git')
+  	repo = GitRepository.new(local_clone_path: 'redmine_remote_git copy')
     refute repo.valid?
   end
 end
